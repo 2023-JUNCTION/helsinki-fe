de67dacb05ecf728dc091e23b93e9d60e6e15106
seung-00 <seung-00@naver.com>
2023-11-12 01:30:14 +0200

    feat: 불필요한 파일 호출 제거

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import Sequence from './pages/Sequence';
 
 export const routes = [
   { path: '/', name: 'Demo', element: <Demo />, nodeRef: createRef() },
-  { path: '/test', name: 'Demo2', element: <Demo2 />, nodeRef: createRef() },
+  // { path: '/test', name: 'Demo2', element: <Demo2 />, nodeRef: createRef() },
   { path: '/sequence', name: 'Sequence', element: <Sequence />, nodeRef: createRef() },
 ];
 
