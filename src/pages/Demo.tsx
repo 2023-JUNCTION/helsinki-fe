0c56ff9e18a029cae496d339f460260ea1602eab
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2023-11-11 19:40:40 +0200

    test

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { API } from '~/api';
 
 const Demo = () => {
@@ -14,20 +14,23 @@ const Demo = () => {
   const [count, setCount] = useState(0);
   // const [beta, setBeta] = useState(0);
 
-  const motionHandler = (event: { accelerationIncludingGravity: any }) => {
-    setGranted(true);
-    const accGravity = event.accelerationIncludingGravity;
-    setYg(accGravity.y);
-    if (Number(accGravity.y) > 8 && !flag) {
-      setCount(prev => prev - 1);
-      setFlag(true);
-    }
-    if (Number(accGravity.y) < -8 && flag) {
-      setCount(prev => prev + 1);
-      setFlag(false);
-      setStep(prev => prev + 1);
-    }
-  };
+  const motionHandler = useCallback(
+    (event: { accelerationIncludingGravity: any }) => {
+      setGranted(true);
+      const accGravity = event.accelerationIncludingGravity;
+      setYg(accGravity.y);
+      if (Number(accGravity.y) > 8 && !flag) {
+        setCount(prev => prev - 1);
+        setFlag(true);
+      }
+      if (Number(accGravity.y) < -8 && flag) {
+        setCount(prev => prev + 1);
+        setFlag(false);
+        setStep(prev => prev + 1);
+      }
+    },
+    [flag],
+  );
 
   function orientationHandler() {
     // event: { beta: any }) {
