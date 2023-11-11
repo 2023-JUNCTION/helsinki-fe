be9c413a4b41d3ed3e93e6250625b33e82ef43de
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2023-11-11 18:54:43 +0200

    test

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -17,10 +17,10 @@ const Demo = () => {
     setGranted(true);
     const accGravity = event.accelerationIncludingGravity;
     setYg(accGravity.y);
-    if (accGravity.y < -8) {
+    if (Number(accGravity.y) < -8) {
       setFlag(true);
     }
-    if (accGravity.y > 8) {
+    if (Number(accGravity.y) > 8) {
       if (flag === true) {
         setStep(prev => prev + 1);
         setFlag(false);
