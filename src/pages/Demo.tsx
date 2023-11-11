8267710dec97f14eaaa4254945730df3ff639c71
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2023-11-11 17:12:02 +0200

    test

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -14,12 +14,7 @@ const Demo = () => {
 
   useEffect(() => {
     (async () => {
-      try {
-        const result = await API.Test.readHealthCheck();
-        console.log(result);
-      } catch (e) {
-        console.log(e, 'error');
-      }
+      await API.Test.readHealthCheck();
     })();
 
     function motionHandler(event: { accelerationIncludingGravity: any }) {
